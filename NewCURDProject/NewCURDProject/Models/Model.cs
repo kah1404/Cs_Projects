f4cf559b09c60a0a767a74b5bea13a399aac8c55
Kevin Andre Hansen <kevin.hansen@abax.no>
2017-11-03 14:18:22 +0100

    fdfd

---
+++ NewCURDProject/NewCURDProject/Models/Model.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewCURDProject.Models
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int PersonId { get; set; }
+        public Person Person { get; set; }
+    }
+}
---
+++ TestSQLDatabase/TestSQLDatabase/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestSQLDatabase.Models;
+
+namespace TestSQLDatabase.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            TestContext db = new TestContext();
+
+            return View(db.MovieList.ToList());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
