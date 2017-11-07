c72d406f1fc8c9c1c35c07a91c7af6fb07afe70d
Kevin Andre Hansen <kevin.hansen@abax.no>
2017-11-07 15:41:34 +0100

    f

---
+++ LinqTest/LinqTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\windows";
+            ShowLargeFilesWithLinq(path);
+            Console.WriteLine("______________");
+            ShowLargeFilesWithoutLinq(path);
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = new DirectoryInfo(path).GetFiles()
+                .OrderByDescending(f => f.Length)
+                .Take(5);
+//            var query = from file in new DirectoryInfo(path).GetFiles()
+//                orderby file.Length descending
+//                select file;
+                
+
+            foreach (var file in query/*.Take(5)*/)
+            {
+                Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+            for (var i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+            }
+        }
+
+        public class FileInfoComparer : IComparer<FileInfo>
+        {
+            public int Compare(FileInfo x, FileInfo y)
+            {
+                return y.Length.CompareTo(x.Length);
+            }
+        }
+    }
+}
